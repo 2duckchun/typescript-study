cd974f4b26f2b23cf1fa3db896ec51a245ee9e77
2DC <kkts9308@gmail.com>
2023-03-12 17:37:03 +0900

    typescript TODOLIST (기초문법 실습)

--- type-dom/dist/index.js
+++ type-dom/dist/index.js
@@ -1,9 +1,41 @@
 "use strict";
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
-btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", () => {
-    console.log(input.value);
+const form = document.querySelector("#todoform");
+const list = document.querySelector("#todoList");
+const readTodos = () => {
+    const todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+};
+const createTodo = (todo) => {
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", () => {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLI.append(todo.text);
+    newLI.append(checkbox);
+    list === null || list === void 0 ? void 0 : list.append(newLI);
+};
+const saveTodos = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+};
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveTodos();
     input.value = "";
-});
-// let greeting: unknown = "Hello World"
-// const greetnum = (greeting as string).length
+};
+const todos = readTodos();
+todos.forEach(createTodo);
+form.addEventListener("submit", handleSubmit);
--- type-dom/src/index.ts
+++ type-dom/src/index.ts
@@ -1,11 +1,49 @@
-const btn = document.getElementById("btn")
+interface Todo {
+  text: string,
+  completed: boolean
+}
+
+const btn = document.getElementById("btn") as HTMLButtonElement
 const input = document.getElementById("todoinput") as HTMLInputElement
+const form = document.querySelector("#todoform") as HTMLFormElement
+const list = document.querySelector("#todoList") as HTMLUListElement
 
-btn?.addEventListener("click", () => {
-  console.log(input.value)
+const readTodos = (): Todo[] => {
+  const todosJSON = localStorage.getItem("todos")
+  if (todosJSON === null) return []
+  return JSON.parse(todosJSON)
+}
+
+const createTodo = (todo: Todo) => {
+  const newLI = document.createElement("li")
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  checkbox.checked = todo.completed
+  checkbox.addEventListener("change", () => {
+    todo.completed = checkbox.checked
+    saveTodos()
+  })
+  newLI.append(todo.text)
+  newLI.append(checkbox)
+  list?.append(newLI)
+}
+
+const saveTodos = () => {
+  localStorage.setItem("todos", JSON.stringify(todos))
+}
+
+const handleSubmit = (e: SubmitEvent) => {
+  e.preventDefault()
+  const newTodo: Todo = {
+    text: input.value,
+    completed: false,
+  }
+  createTodo(newTodo)
+  todos.push(newTodo)
+  saveTodos()
   input.value = ""
-})
+}
 
-
-// let greeting: unknown = "Hello World"
-// const greetnum = (greeting as string).length
+const todos: Todo[] = readTodos()
+todos.forEach(createTodo)
+form.addEventListener("submit", handleSubmit)
