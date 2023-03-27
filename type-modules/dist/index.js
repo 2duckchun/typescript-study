ba0cb448c3b999c84453dde3999180dc366bb724
2DC <kkts9308@gmail.com>
2023-03-27 22:25:48 +0900

    타입스크립트 모듈 공부

---
+++ type-modules/dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("./utils");
+(0, utils_1.sample)([12, 3, 4]);
+(0, utils_1.add)(1, 2);
---
+++ type-modules/src/index.ts
@@ -0,0 +1,3 @@
+import { add, sample } from "./utils"
+sample([12, 3, 4])
+add(1, 2)
